0f2cb04b39d82e0f3fbfbf32fbf634267664ba60
Neela Hanumanthiah <neelahanumanthiah@mu-584783.dhcp.missouri.edu>
2024-02-08 16:58:29 -0600

    Implemented a method that searches a studnet by their id, and displays if they are found(returns true) or not(returns false)

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -1,9 +1,13 @@
 package hw2;
 
+import hw2.students.StudentManager;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		StudentManager studentManager = new StudentManager();
+		boolean fileReadStatus = studentManager.readFromFile("studentsData.txt");
 
 	}
 
