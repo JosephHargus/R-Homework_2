51af8ce0e97dac33a11c1a122ebe711c47ac7d34
riannaellis <riannaellis@pcp003238pcs.dhcp.missouri.edu>
2024-02-08 16:02:17 -0600

    Developed readFromFile() method in StudentManager.java
    I created this method to read the data from a file using FileInputStream
    and Scanner. Then, using the data from each line in the file, it creates
    a new Student object with their id, name, and grade from the file. The
    method returns false if it encounters any exceptions.

--- src/hw2/students/StudentManager.java
+++ src/hw2/students/StudentManager.java
@@ -1,6 +1,9 @@
 package hw2.students;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentManager {
 	
@@ -11,7 +14,25 @@ public class StudentManager {
 	}
 
 	public boolean readFromFile(String fileName) {
-		return false;
+		try {
+			FileInputStream file = new FileInputStream(fileName);
+			Scanner fileContents = new Scanner(file);
+			
+			while(fileContents.hasNext()) {
+				String studentInfo = fileContents.nextLine();
+				String[] studentInfoArray = studentInfo.split(" ");
+				int id = Integer.parseInt(studentInfoArray[0]);
+				String name = studentInfoArray[1] + " " + studentInfoArray[2];
+				double grade = Double.parseDouble(studentInfoArray[3]);
+				Student newStudent = new Student(id, name, grade); 
+				students.add(newStudent);
+			}
+			return true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public void displayStudents() {
@@ -24,5 +45,5 @@ public class StudentManager {
 	
 	public boolean updateStudentGradeById(int id, double grade) {
 		return false;
-	}
+	}	
 }
